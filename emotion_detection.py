b091deb7d4d62ccf00b9325c74154cfed0491082
Simon-E <mgsimon635@gmail.com>
2025-05-31 15:07:51 -0400

    Format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,13 +1,35 @@
-import requests
 import json
+import requests
 
 def emotion_detector(text_to_analyse):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
-
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
+    headers = {
+        "Content-Type": "application/json",
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
 
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    response = requests.post(url, json=myobj, headers=headers)
 
-    response = requests.post(url, json=myobj, headers=header)
+    if response.status_code != 200:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
-    return response.text
+    result = response.json()
+    emotions = result['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
